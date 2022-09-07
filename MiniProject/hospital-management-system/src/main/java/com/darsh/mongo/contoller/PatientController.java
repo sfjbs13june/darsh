019ccef8a982196f4c1a55f9ac76fdedbeda181c
sfjbs13june <sfjbs13june@gmail.com>
2022-09-07 08:57:14 +0000

    Mini Project (hospital-management-system) By Darsh Jotangia

---
+++ MiniProject/hospital-management-system/src/main/java/com/darsh/mongo/contoller/PatientController.java
@@ -0,0 +1,26 @@
+package com.darsh.mongo.contoller;
+
+import com.darsh.mongo.model.Appointment;
+import com.darsh.mongo.repository.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patient")
+public class PatientController {
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    @GetMapping("/myappointment")
+    public List<Appointment> getMyAppointments(@RequestParam String patientName){
+        return appointmentRepository.findByPatientName(patientName);
+    }
+
+    @PostMapping("/save")
+    public Appointment saveAppointment(@RequestBody Appointment appointment){
+        return appointmentRepository.save(appointment);
+    }
+}
